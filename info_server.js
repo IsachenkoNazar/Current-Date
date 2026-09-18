6f0d3137de66cb500bd34e0714d43bc7d708c9f9
Nazar Isachenko <nazarisacenko@gmail.com>
2026-09-18 21:37:48 +0300

    i finally got it

--- info_server.js
+++ info_server.js
@@ -1,3 +1,4 @@
+
 const express = require("express")
 const moment = require("moment")
 
@@ -8,7 +9,9 @@ const PORT = 5000
 const ENDPOINT = "timestamp/uptime/nodeVersion"
 const ENDPOINT1 = "status"
 
-const information = express() //
+const information = express()
+
+information.use(express.json())
 
 const products = [
   {
@@ -43,39 +46,90 @@ const products = [
   }
 ]
 
+
 information.get('/products', (req, res) => {
-    const { category, take } = req.query;
+  const { category, take, fail } = req.query;
 
-    let result = category 
-        ? products.filter(p => p.category === category) 
-        : [...products];
-
-    if (take) {
-        const takeLimit = parseInt(take, 10);
-        if (!isNaN(takeLimit) && takeLimit > 0) {
-            result = result.slice(0, takeLimit);
-        }
-    }
-
-    res.status(200).json({
-        products: result
+  if (fail === "true") {
+    return res.status(500).json({
+      ok: false,
+      error: "Our own error"
     });
+  }
+  
+  let result = category 
+  ? products.filter(p => p.category === category) 
+  : [...products];
+  
+  if (take) {
+    const takeLimit = parseInt(take, 10);
+    if (!isNaN(takeLimit) && takeLimit > 0) {
+      result = result.slice(0, takeLimit);
+    }
+  }
+  
+  res.status(200).json({
+    products: result
+  });
 });
 
 
 information.get('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id, 10);
-    const product = products.find(p => p.id === productId);
+  const productId = parseInt(req.params.id, 10);
+  const product = products.find(p => p.id === productId);
+  
+  if (!product) {
+    return res.status(404).json({
+      ok: false,
+      description: "Product not found."
+    });
+  }
+  
+  res.status(200).json(product);
+});
 
-    if (!product) {
-        return res.status(404).json({
-            ok: false,
-            description: "Product not found."
-        });
+
+information.post('/products', async (req, res) => {
+  const {name, price, category, image} = req.body
+  const {fail} = req.query
+  try {  
+    if ((typeof name != "string" || name.trim().length == 0) || (!Number.isInteger(price) || price <= 0) || (typeof category !== "string" || category.trim().length === 0)) {
+      return res.status(422).json({ok: false, description: "Valid Error"})
+    }
+    if ((products.some(product => product.name === name))){
+      return res.status(409).json({ok: false, description: "Product with that name already exsist"})
+    }
+    
+    const newProduct = {
+      id: products.length + 1,
+      name: name,
+      price: price,
+      category: category
+    }
+    
+    function addProduct(newProduct) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (fail === "true") {
+            return reject(new Error("Our own Error"))
+          }
+          products.push(newProduct)
+          resolve(newProduct)
+        }, 1000)
+      })
     }
 
-    res.status(200).json(product);
-});
+    await addProduct(newProduct)
+    res.status(201).json({ok: true, product: newProduct})
+
+  } catch {
+    return res.status(500).json({
+      ok: false,
+      error: "Our own error"
+    })
+  }
+})
+
 
 information.get(`/${ENDPOINT}`, (req, res) => {
   res.json({
@@ -96,4 +150,5 @@ information.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}/${ENDPOINT}`);
   console.log(`Check your status on http://${HOST}:${PORT}/${ENDPOINT1}`);
   console.log(`Products list on http://${HOST}:${PORT}/products`);
-});
+  console.log(`Products list  on http://${HOST}:${PORT}/products?fail=true`);
+})
